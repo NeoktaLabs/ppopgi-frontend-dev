2f5909d8cf4a5d9f59dcadac5b1c2b0de5976088
Julien - NeoktaLabs <74187584+NeoktaLabs@users.noreply.github.com>
2026-01-15 17:33:09 +0100

    Update main.tsx

--- ppopgi-frontend/src/main.tsx
+++ ppopgi-frontend/src/main.tsx
@@ -1,13 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { WalletProviders } from "./lib/wallet";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <WalletProviders>
-      <App />
-    </WalletProviders>
-  </React.StrictMode>
+  <WalletProviders>
+    <App />
+  </WalletProviders>
 );
