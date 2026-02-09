0900f0629f2d32f4428d17ed2b45b0fb8ede372a
Julien - NeoktaLabs <74187584+NeoktaLabs@users.noreply.github.com>
2026-02-09 13:00:33 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  // Some ledger deps reference `global`
+  define: {
+    global: "globalThis",
+  },
+
+  // Make sure Buffer is injected during dependency pre-bundling
+  optimizeDeps: {
+    include: ["buffer", "@ledgerhq/hw-transport-webhid", "@ledgerhq/hw-app-eth"],
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+      inject: [resolve("node_modules/buffer/index.js")],
+    },
+  },
+});
