80514af2bc9f0f67854571b4a4589e8672a7f0b6
Julien - NeoktaLabs <74187584+NeoktaLabs@users.noreply.github.com>
2026-02-09 14:36:22 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,19 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  define: {
+    global: "globalThis",
+  },
+
+  resolve: {
+    alias: {
+      buffer: "buffer",
+      process: "process/browser",
+    },
+  },
+
+  optimizeDeps: {
+    include: ["buffer", "process"],
+  },
 });
