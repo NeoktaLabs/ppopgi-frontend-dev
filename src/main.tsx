2ae862f0149c9d5499f31894349f05cec613abc1
Julien - NeoktaLabs <74187584+NeoktaLabs@users.noreply.github.com>
2026-02-12 21:07:28 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -11,10 +11,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ThirdwebProvider } from "thirdweb/react";
+import { thirdwebClient } from "./thirdweb/client";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThirdwebProvider>
+    <ThirdwebProvider client={thirdwebClient}>
       <App />
     </ThirdwebProvider>
   </StrictMode>
